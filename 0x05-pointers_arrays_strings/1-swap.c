26b9e7662b065278ddb266f173ebf55ca344b6c7
smolworld <faithmwanza64@gmail.com>
2023-08-23 02:25:14 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps value of two integer
+ * @a: first value
+ * @b: second value
+ */
+void swap_int(int *a, int *b);
+{
+int z
+z = *a;
+*a = *b;
+*b = z;
+}
