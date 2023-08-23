35f8ff938a54c895c8baedd79e783ea46e8d1646
smolworld <faithmwanza64@gmail.com>
2023-08-23 03:39:07 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _strlen - function that returns the length of a string
+ * @s : String
+ * Return : Length of string
+ */
+int _strlen(char *s);
+{
+int c = 0;
+for (*s != '\0'; s++)
+{
+
+c++;
+}
+return (c);
+
+}
